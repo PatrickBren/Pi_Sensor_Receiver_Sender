248a72651f79bc7aee834201456b077daf7b962d
Patrick Brennan <43745096+PatrickBren@users.noreply.github.com>
2019-02-04 11:11:43 +0000

    Version 0.2.0

--- dweep_firebase.py
+++ dweep_firebase.py
@@ -1,6 +1,8 @@
 from firebase import firebase
 import time
 import datetime
+from threading import Thread
+
 # from grovepi import *
 # import grovepi
 
@@ -13,69 +15,107 @@ light_sensor = 1 #connect the Light sensor into the A1 analog port
 dht_sensor_port = 7 # connect the DHt sensor to port 7
 dht_sensor_type = 0 # use 0 for the blue-colored sensor and 1 for the white-colored sensor
 
-while True:
-    try:
-        #TimeStamp Method
-        def getTime():
-            ts = time.time()
-            stamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-            return stamp
+
+##Have Different Threads that listen for any change and automaticaly change the variables for speed and on/off
+
+
+#TimeStamp Method
+def getTime():
+    ts = time.time()
+    stamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+    return stamp
             
         
-        #Gets the value from the sound sensor
-        def getSound():
-            sound_value = {"time": getTime(),
-                           "data": 1 }#grovepi.analogRead(sound_sensor)
-            time.sleep(1)
-            return sound_value
+#Gets the value from the sound sensor
+def getSound():
+    speed = firebase.get('sensors/sound', 'speed')
+    if firebase.get('sensors/sound', 'running') == True:
+       
+        sound_value = {"time": getTime(), "data": 1 }#grovepi.analogRead(sound_sensor)
         
-        #Gets the value from the light sensor
-        def getLight():
-            light_value = {"time": getTime(),
-                           "data": 1 }#grovepi.analogRead(light_sensor)
-            time.sleep(1)
-            return light_value
+        firebase.post('sensors/sound/sensor-data', sound_value)
+        
+        print("Sound Sensor Value added")
+    else:
+        print "Sound Sensor isn't running"
+    time.sleep(speed)
+    return
             
-        #Gets the value of temperature and humidity but only returns temperature
-        def getTemp():
-            temp = {"time": getTime(),
-                           "data": 1 }#dht(dht_sensor_port,dht_sensor_type)
-            time.sleep(1)
-            return temp
+        
+
+#Gets the value from the light sensor
+def getLight():
+    speed = firebase.get('sensors/light', 'speed')
+    if firebase.get('sensors/light', 'running') == True:
+        
+        light_value = {"time": getTime(),"data": 1 }#grovepi.analogRead(light_sensor)
+        
+        firebase.post('sensors/light/sensor-data', light_value)
+        
+        print "Light Sensor Value added"
+    else:
+        print "Light Sensor isn't running"
+    time.sleep(speed)
+    return
             
-        #Gets the value of temperature and humidity but only returns humidity
-        def getHumidity():
-            hum = {"time": getTime(),
-                           "data": 1 }#dht(dht_sensor_port,dht_sensor_type)
-            time.sleep(1)
-            return hum
+        
+#Gets the value of temperature and humidity but only returns temperature
+def getTemp():
+    speed = firebase.get('sensors/temp', 'speed')
+    if firebase.get('sensors/temp', 'running') == True:
+        
+        temp = {"time": getTime(),"data": 1 }#dht(dht_sensor_port,dht_sensor_type)
+        
+        firebase.post('sensors/temp/sensor-data', temp)
+        
+        print "Temp Sensor Value added"
+    else:
+        print "Temp Sensor isn't running"
+    time.sleep(speed)
+    return
+
             
-        #Sound Sensor
-        if firebase.get('sensors/sound', 'running') == True:
-            firebase.post('sensors/sound/sensor-data', getSound())
-        else:
-            print "Sound Sensor isn't running"
         
-        #Light Sensor
-        if firebase.get('sensors/light', 'running') == True:
-            firebase.post('sensors/light/sensor-data', getLight())
-        else:
-            print "Light Sensor isn't running"
+#Gets the value of temperature and humidity but only returns humidity
+def getHumidity():
+    speed = firebase.get('sensors/humidity', 'speed')
+    if firebase.get('sensors/humidity', 'running') == True:
         
-        #Temp Sensor 
-        if firebase.get('sensors/temp', 'running') == True:
-            firebase.post('sensors/temp/sensor-data', getTemp())
-        else:
-            print "Temp Sensor isn't running"
+        hum = {"time": getTime(),"data": 1 }#dht(dht_sensor_port,dht_sensor_type
         
-        #Humidity Sensor
-        if firebase.get('sensors/humidity', 'running') == True:
-            firebase.post('sensors/humidity/sensor-data', getHumidity())
-        else:
-            print "Humidity Sensor isn't running"
+        firebase.post('sensors/humidity/sensor-data', hum)
         
-        
-        print "Success"
-        time.sleep(5)
-    except:
-        print "An Error occurred"
+        print "Humidity Sensor Value added"
+    else:
+        print "Humidity Sensor isn't running"
+    time.sleep(speed)
+    return
+
+
+class Sound_Sensor(Thread):
+    def run(self):
+        while True:
+            getSound();
+
+class Light_Sensor(Thread):
+    def run(self):
+        while True:
+            getLight();
+            
+class Temp_Sensor(Thread):
+    def run(self):
+        while True:
+            getTemp();
+            
+class Humidity_Sensor(Thread):
+    def run(self):
+        while True:
+            getHumidity();
+            
+def run():
+    Sound_Sensor().start()
+    Light_Sensor().start()
+    Temp_Sensor().start()
+    Humidity_Sensor().start()
+    
+run()
